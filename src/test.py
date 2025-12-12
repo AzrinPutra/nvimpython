88b80fbff3b3ee539f660baec82a393aa4c9723d
AzrinPutra <i.am.azrin@gmail.com>
2025-12-12 12:36:43 +0800

    (NewFile) Python Solving Skills on GFG
    Practise skills on GFG

---
+++ src/test.py
@@ -0,0 +1,22 @@
+# ==== Find the largest int in a array ====
+# class Solution:
+#    def largest(self, arr):
+#       largest_int = max(arr)
+#        print(f"{largest_int}")
+
+
+# array1 = [1, 65, 8, 100]
+# Solution().largest(array1)
+
+# ==== Find the index of a given element if it is found in the array ====
+# ==== If not, return -1 ====
+# class Solution:
+#   def search(self, arr, x):
+#       for i in range(len(arr)):
+#           if arr[i] == x:
+#               return i
+#       return -1
+
+
+# array = [45, 68, 9000, 200, 5]
+# Solution().search(array, x=9000)

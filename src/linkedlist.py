6c2bfa2dba1d9d928155a106128eff0115b45175
AzrinPutra <i.am.azrin@gmail.com>
2025-12-23 10:34:39 +0800

    (Update) linkedlist.py
    basic structure for singely linked list

--- src/linkedlist.py
+++ src/linkedlist.py
@@ -1,4 +1,23 @@
-class node:
+class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
+
+
+# create Nodes
+node1 = Node(15)
+node2 = Node(45)
+node3 = Node(32)
+
+# Link nodes
+node1.next = node2
+node2.next = node3
+
+# head node
+head = node1
+
+current = head
+while current:
+    print(current.data, end="-->")
+    current = current.next
+print("None")
